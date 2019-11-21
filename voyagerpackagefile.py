61aff39967be8e35b80582fb40c1fa0f8c5608ff
Stephan Iskandarani <stephan.iskandarani@prodrive-technologies.com>
2019-11-21 10:18:28 +0100

    Allow packages without valid semver to still be included in projects

--- voyagerpackagefile.py
+++ voyagerpackagefile.py
@@ -16,12 +16,13 @@ class VoyagerPackageFile():
 
     def add_dependencies(self, deps):
         for d in deps:
-            v = semver.valid(d['version'], False)
+            print(d['version'])
+            v = semver.clean(d['version'], False)
             if v:
                 # Pin to the major and minor version
                 d['version'] = f"{v.major}.{v.minor}"
             else:
-                raise ValueError(f"The dependency {d['library']} has a non valid version {d['version']} for release")
+                print(f"WARNING: The dependency {d['library']} has a non valid version '{d['version']}' for release")
             # Rename dependency_type to type for package file
             d['type'] = d['dependency_type']
             del d['dependency_type']
