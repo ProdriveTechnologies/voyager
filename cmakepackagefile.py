a6333690989e4ec6e6a581a160ea54e3fe9f2d9e
Dominic van Berkel <dominic.van.berkel@prodrive-technologies.com>
2019-12-06 17:51:13 +0100

    Write include paths and link_lib to CMakeLists.txt

--- cmakepackagefile.py
+++ cmakepackagefile.py
@@ -1,14 +1,36 @@
 from buildinfo import Package
 
 class CMakePackageFile:
-  def __init__(self, package: Package):
-    self.package = package
+    def __init__(self, package: Package):
+        self.package = package
+        self.path = package.rootpath + 'CMakeLists.txt'
 
-  def save(self):
-    text = '''add_library({safe_name}-{version} INTERFACE)'''.format(
-      safe_name = self.package.safe_name,
-      version = self.package.version
-    )
+    def save(self):
+        template = \
+'''add_library({package_name} INTERFACE)
 
-    with open(self.package.rootpath + 'CMakeLists.txt', 'w') as cmake:
-      cmake.write(text)
+target_include_directories({package_name} INTERFACE
+  {include_dirs}
+)
+
+target_link_libraries({package_name} INTERFACE
+  {libs}
+)
+'''
+
+        package_name = '{}-{}'.format(self.package.safe_name, self.package.version)
+        include_dirs = '\n  '.join(
+            ['"${{CMAKE_CURRENT_SOURCE_DIR}}/{}"'.format(dir) for dir in self.package.include_dirs]
+        )
+        libs = '\n  '.join(
+            ['"${{CMAKE_CURRENT_SOURCE_DIR}}/{}"'.format(lib) for lib in self.package.libs]
+        )
+
+        text = template.format(
+            package_name=package_name,
+            include_dirs=include_dirs,
+            libs=libs
+        )
+
+        with open(self.path, 'w') as cmake:
+            cmake.write(text)
