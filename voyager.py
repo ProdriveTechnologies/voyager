78fb100992ad6b80d961b32edecee6a4ed95ecaf
Maarten Tamboer <maarten.tamboer@prodrive-technologies.com>
2019-12-19 09:09:16 +0100

    Bump version

--- voyager.py
+++ voyager.py
@@ -6,7 +6,7 @@ import random
 import json
 from pathlib import Path
 
-VERSION = "1.6.1"
+VERSION = "1.7.0"
 
 from voyagerfile import VoyagerFile
 from generators.visualstudio import VisualStudioGenerator
