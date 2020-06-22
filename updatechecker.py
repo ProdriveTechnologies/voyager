60a2ce319387f0eee3e9d946ec37b8724021fd32
Maarten Tamboer <maarten.tamboer@prodrive-technologies.com>
2020-06-22 12:58:35 +0200

    Implement async update check

--- updatechecker.py
+++ updatechecker.py
@@ -1,15 +1,32 @@
 
+import threading
 import semver
+import click
 from artifactdownloader import ArtifactDownloader
 
 class UpdateChecker:
     def __init__(self):
-        pass
+        self._update_available = False
+        self._new_version = None
+        self._thread = None
 
     def check_for_update(self, current_version: str):
         down = ArtifactDownloader([], False)
         vers = down.find_versions_for_package("siatd-generic-local", "Tools/voyager", ["win-setup"])
-        for ver in vers:
-            if semver.lt(current_version, ver, False):
-                print(f"New version out: {ver}")
-                break
+        latest_version = semver.max_satisfying(vers, "*", True)
+        if semver.lt(current_version, latest_version, False):
+            self._update_available = True
+            self._new_version = latest_version
+
+    def check_for_update_in_background(self, current_version: str):
+        # set daemon to true to Allow Python to immediately kill the thread on exit
+        self._thread = threading.Thread(target=self.check_for_update, args=(current_version,), daemon=True)
+        self._thread.start()
+
+    def print_result(self):
+        if self._thread is not None:
+            self._thread.join()
+
+        if self._update_available:
+            click.echo(click.style(f"New version of voyager is available ({self._new_version}) please update",
+                                   fg='yellow'))
