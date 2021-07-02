0d76555617486c28dbb2b0b825df5ad86d67a825
Dominic van Berkel <dominic.van.berkel@prodrive-technologies.com>
2021-07-02 14:18:12 +0200

    Add copyright headers

--- voyager/plugin_registry.py
+++ voyager/plugin_registry.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Prodrive Technologies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import importlib
 import pkgutil
 from abc import ABC, abstractmethod, abstractproperty
@@ -112,7 +126,6 @@ class Registry(metaclass=SingletonType):
 # - https://github.com/pyinstaller/pyinstaller/issues/1905#issuecomment-525221546
 #
 
-
 def iter_namespace(ns_pkg):
     """Pyinstaller-compatible namespace iteration.
 
@@ -156,11 +169,7 @@ def load_plugin(plugin: Type[Plugin]):
 
 def load_plugins():
     """
-    Load plugins by importing all packages that start with "voyager_".
-
-    Borrowed from 
-
-    Big TODO: provide diagnostic info when importing or registration fails
+    Load plugins by importing all packages in the voyager_plugins namespace.
     """
     for name in iter_namespace(voyager_plugins):
         plugin = importlib.import_module(name).Plugin
