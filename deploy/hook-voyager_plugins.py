3134093c37b25e29c30b4f6e8443a7c6216e04eb
Dominic van Berkel <dominic.van.berkel@prodrive-technologies.com>
2021-07-02 14:21:51 +0200

    Sync pyinstaller specs

--- deploy/hook-voyager_plugins.py
+++ deploy/hook-voyager_plugins.py
@@ -1,5 +1,5 @@
 # Because plugins are not directly imported, this module hook adds all modules
-# found under the voyager.plugins namespace to the hidden imports.
+# found under the voyager_plugins namespace to the hidden imports.
 
 from PyInstaller.utils.hooks import collect_submodules
 hiddenimports = collect_submodules('voyager_plugins')
