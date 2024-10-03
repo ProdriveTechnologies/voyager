813017652bba6f5df8d13ed8d8d2a0196e870013
Thomas van Beeck <thomas.van.beeck@prodrive-technologies.com>
2024-10-03 13:03:09 +0200

    Use python 3.10 (Prodrive standard)

--- cli.py
+++ cli.py
@@ -17,4 +17,4 @@ import voyager.voyager
 # The goal of this file is to wrap the application for pyinstaller
 # https://stackoverflow.com/a/22566757
 if __name__ == '__main__':
-    voyager.voyager.main() 
+    voyager.voyager.main()
